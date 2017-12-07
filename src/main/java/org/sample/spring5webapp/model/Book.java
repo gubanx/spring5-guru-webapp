563de843a3e09e44f0c769fc3023e9a68707153c
guban <gubanx@gmail.com>
2017-12-07 01:42:32 +0100

    Se añaden métodos equals, hashCode y toString a las clases del modelo

--- src/main/java/org/sample/spring5webapp/model/Book.java
+++ src/main/java/org/sample/spring5webapp/model/Book.java
@@ -35,6 +35,32 @@ public class Book {
         this.authors = authors;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
     public Long getId() {
         return id;
     }
